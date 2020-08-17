1a75c9efcbce70e653b2aedab3d34c5f484a662a
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2020-08-17 20:47:12 -0300

    Initial commit, setting up sockets

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
