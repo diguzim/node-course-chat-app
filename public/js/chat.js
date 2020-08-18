ec98cbdf38c10f8cd41917d1f2229ca54827b95d
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2020-08-18 00:58:10 -0300

    Sharing position and starting to acknowledge messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,5 +9,20 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (message) => {
+        console.log('The message was delivered!', message)
+    })
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+    
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,9 +17,21 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined in')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
         io.emit('message', message)
+        callback('Delivered!')
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (location) => {
+        console.log('away')
+        console.log(location)
+        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`)
     })
 })
 
